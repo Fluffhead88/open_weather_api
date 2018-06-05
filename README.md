a72b35eeea08a5052d8507de19ce64f77d89e15e
Fluffhead88 <thigpen.zach@gmail.com>
2018-06-04 23:34:11 -0400

    ready for review

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # open_weather_api
+
+Get the current weather for your home town.
+Print out a nicely-formatted summary of the five day forecast for a city near your dream vacation.
+For an extra degree of difficulty, tell the user (you) what they should wear in the following conditions: Hot, Comfortable, Cold, Rain, and Snow.
--- dream_city_5day.py
+++ dream_city_5day.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 
 api_key = 'be719e8432aa621a508a5bba848e6bfe' # Put your API key in this string.
 
@@ -6,8 +7,11 @@ test_url = 'http://api.openweathermap.org/data/2.5/forecast?q=Athens,us&APPID=' 
 resp = requests.get(test_url)
 if resp.status_code in [200, 201]:
     weather_data = resp.json()
-    """print("The weather in {} is {}.".format(weather_data['weather'][0]['description']))
+
+    my_data = weather_data['list']
+
+    for x in my_data[0::4]:
+        print("In {}, GA the weather is {} and the temperature is {} degrees on {}.\n".format(weather_data['city']['name'], x['weather'][0]['description'], x['main']['temp'], x['dt_txt']))
+
 else:
-    print("ERROR: " + str(resp.status_code))"""
-    for list in weather_data:
-        print('name')
+    print("ERROR: " + str(resp.status_code))
